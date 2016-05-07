c05b8b382f3a763288b7c9bbf41cb97770ec18aa
Fei Wang <u5575182@anu.edu.au>
2016-05-07 22:06:25 +1000

    init project

---
+++ src/main/java/feedme/feedme_socket/App.java
@@ -0,0 +1,13 @@
+package feedme.feedme_socket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
